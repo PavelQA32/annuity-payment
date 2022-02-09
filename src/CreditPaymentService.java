cd400f5fc19dd6c76297138c875c633ef86723ae
PS <ps-667@mail.ru>
2022-02-09 19:40:11 +0300

    Syntax

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -7,18 +7,5 @@ public class CreditPaymentService {
 
     }
 
-//    public class Main {
-//        public static void main(String[] args) {
-//            int loanAmount = 1_000_000;
-//            double interestRate = 9.99;
-//            int creditTerm = 12;
-//            int percent = 100;
-//            double loanRatePerMonth = interestRate / 12 / 100;
-//            double annuityRatio = (loanRatePerMonth * Math.pow((1+loanRatePerMonth),creditTerm)) / ((Math.pow((1+loanRatePerMonth),creditTerm))-1);
-//            double annuityPayment = annuityRatio * loanAmount;
-//
-//            System.out.println(annuityPayment);
-//
-//        }
-//    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -9,17 +9,5 @@ public class Main {
 
         double annuityPaymentForThreeYears = service.calculate(1_000_000, 9.99, 36, 100);
         System.out.println(annuityPaymentForThreeYears);
-
-
-//        int loanAmount = 1_000_000;
-//        double interestRate = 9.99;
-//        int creditTerm = 12;
-//          int percent = 100;
-//        double loanRatePerMonth = interestRate / creditTerm / percent;
-//        double annuityRatio = (loanRatePerMonth * Math.pow((1+loanRatePerMonth),creditTerm)) / ((Math.pow((1+loanRatePerMonth),creditTerm))-1);
-//        double annuityPayment = annuityRatio * loanAmount;
-//
-//        System.out.println(annuityPayment);
-
     }
 }
